8e486f513630f602f3c576e3d92c7f975ada5935
Kinga Buczniewska <kingabuczniewska@gmail.com>
2021-05-11 23:10:53 +0200

    delete comments

--- js/script.js
+++ js/script.js
@@ -31,25 +31,12 @@
     const onFormImput = () => {
         const amountElement = document.querySelector(".js-amount")
         const currencyConvertElement = document.querySelector(".js-currencyConvert")
-        // const currencyElement = document.querySelector(".js-currency")
 
 
-        // var rates = {
-        //     "EUR": 4.55,
-        //     "GBP": 5.26,
-        //     "CHF": 4.13,
-        //     "USD": 3.78
-        // }
-
-        // var przelicznik = rates[currencyConvertElement.value]
-        // console.log(przelicznik)
-
-
-        var received = receivedResult(amountElement, currencyConvertElement);
+        let received = receivedResult(amountElement, currencyConvertElement);
         updateReceived(received);
 
     };
-
 
     const init = () => {
         const formElement = document.querySelector(".js-form")
